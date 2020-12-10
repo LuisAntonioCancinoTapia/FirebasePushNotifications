c3b33af7e38b39244d42830d0f451b8bac8a34f1
LuisAntonioCancinoTapia <luicancinot@upt.pe>
2020-12-10 09:57:02 -0500

    Firebase Push Notifications

---
+++ app/src/main/java/com/example/firebasepushnotifications/Fcm.java
@@ -0,0 +1,81 @@
+package com.example.firebasepushnotifications;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class Fcm extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.e("token", "mi token es: " + s);
+        guardarToken(s);
+    }
+
+    private void guardarToken(String s){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("token");
+        reference.child("cancino").setValue(s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String from = remoteMessage.getFrom();
+
+        if (remoteMessage.getData().size() > 0){
+            String titulo = remoteMessage.getData().get("titulo");
+            String detalle = remoteMessage.getData().get("detalle");
+            mayorqueoreo(titulo,detalle);
+        }
+    }
+
+    public void mayorqueoreo(String titulo, String detalle){
+        String id = "mensaje";
+
+        NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, id);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel nc = new NotificationChannel(id,"nuevo",NotificationManager.IMPORTANCE_HIGH);
+            nc.setShowBadge(true);
+            assert nm != null;
+            nm.createNotificationChannel(nc);
+        }
+
+        builder.setAutoCancel(true)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle(titulo)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(detalle)
+                .setContentIntent(clickNoti())
+                .setContentInfo("nuevo");
+        Random random = new Random();
+        int idNotify = random.nextInt(8000);
+
+        assert nm != null;
+        nm.notify(idNotify,builder.build());
+    }
+
+    public PendingIntent clickNoti(){
+        Intent nf = new Intent(getApplicationContext(), MainActivity.class);
+        nf.putExtra("color","rojo");
+        nf.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        return PendingIntent.getActivity(this,0,nf,0);
+    }
+}
